d1c95dd969d23979cc395c98e7aa4d8f9eebb4f3
Matthijs Raatgever <matthijs4004@gmail.com>
2021-10-27 11:44:09 +0200

    remove spelProgramma.py

--- spelProgramma.py
+++
@@ -1,8 +0,0 @@
-import random
-
-spelList = ['Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo']
-
-def spelProgramma(spelList):
-    print(random.choice(spelList))
-
-spelProgramma(4)
