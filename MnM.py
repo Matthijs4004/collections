8a54f6418f40d6962060724aaadad69a105c2519
Matthijs4004 <matthijs4004@gmail.com>
2021-11-03 09:33:46 +0100

    add Dictionary part

--- MnM.py
+++ MnM.py
@@ -1,12 +1,29 @@
 import random
 
-mnmlist = ["groen", "oranje", "blauw", "bruin"]
+#Deel 1: Lists
+
+mnmList = ["groen", "oranje", "blauw", "bruin"]
 
 print("Hoeveel kleuren m&m's wil je aan de zak toevoegen?")
 aantal = int(input("> "))
 
 def listZak(x):
-  print(random.choices(mnmlist, k=x))
+  print(random.choices(mnmList, k=x))
 
 listZak(aantal)
 
+# Deel 2: Dictionary
+
+mnmDict = {}
+
+def dictZak(aantal:int):
+  for i in range(aantal):
+    mnmColor = random.randint(0,3)
+    if not mnmList[mnmColor] in mnmDict:
+      mnmDict[mnmList[mnmColor]] = 1
+    else:
+      mnmDict[mnmList[mnmColor]] += 1
+  return mnmDict
+
+
+print(dictZak(aantal))
