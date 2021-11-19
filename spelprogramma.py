96469350abec783d448ec7ef1fb84fc508c64907
Matthijs4004 <matthijs4004@gmail.com>
2021-11-19 10:24:30 +0100

    add minimum to spelprogramma.py

--- spelprogramma.py
+++ spelprogramma.py
@@ -2,8 +2,8 @@ import random
 
 spelList = ['Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo']
 
-def spelProgramma(spelList):
-    number = random.randrange(3,11)
+def spelProgramma(spelList, minimum):
+    number = random.randrange(minimum,11)
     print(random.choices(spelList, k = number))
 
 spelProgramma(spelList)
