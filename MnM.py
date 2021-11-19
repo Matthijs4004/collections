4f5e72b43e1f4de3e54a591e2f59411beab2cb02
Matthijs4004 <matthijs4004@gmail.com>
2021-11-19 10:17:07 +0100

    add dagen.py

--- MnM.py
+++ MnM.py
@@ -16,14 +16,22 @@ listZak(aantal)
 
 mnmDict = {}
 
-def dictZak(aantal:int):
-  for i in range(aantal):
-    mnmColor = random.randint(0,3)
-    if not mnmList[mnmColor] in mnmDict:
+def dictZak(aantal:int): #function met parameter -> int
+  for i in range(aantal): 
+    mnmColor = random.randint(0,3) #random number generator tussen 0-3
+    if not mnmList[mnmColor] in mnmDict: #mnmcolor geeft een nummber tussen 0-3 en die wordt gecheckt door de list, als die niet in de dict zit wordt die toegevoegd zo niet dat wordt er wat bij gedaan
       mnmDict[mnmList[mnmColor]] = 1
     else:
       mnmDict[mnmList[mnmColor]] += 1
-  return mnmDict
-
+  return mnmDict #return de mnmdict zodat hij de juiste output geeft en niet 'none'
 
 print(dictZak(aantal))
+
+# Deel 3: sorteren
+
+def mnmSort(sorteer):
+    print(sorteer)
+
+
+
+mnmSort(dictZak(aantal))
---
+++ dagen.py
@@ -0,0 +1,11 @@
+
+dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"]
+
+
+print(dagen)
+print(dagen[:5])
+print(dagen[-2:])
+dagen.reverse()
+print(dagen)
+print(dagen[-5:])
+print(dagen[:2])
